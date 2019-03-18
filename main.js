b82f4451621914104e7f22a8e3eb1c436fa3f4e0
abinonayq <abinonayq@elm.sa>
2019-03-18 16:15:50 +0300

    fullcalender

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function () { //must always be here if you use JQuery
 
     let db = firebase.firestore().collection('resturants')
     let resList = $('.res-container')
+    let table = $('#data-grid')
+
 
     var tdate = new Date();
     var dd = tdate.getDate();
@@ -12,14 +14,40 @@ $(document).ready(function () { //must always be here if you use JQuery
 
     console.log(currentDate)
 
-    db.get().then(result => {
-        let changes = result.docChanges() //gets array of docs
-        console.log(changes)
-        changes.forEach(res => {
-            console.log(res.doc.data());
-            resList.append(`<li data-id="${res.doc.id}" class="li-look d-flex justify-content-lg-between px-3 py-1"> <span class="nameClass">${res.doc.data().name} </span> - <span class="locationClass"> ${res.doc.data().location} </span> - <span class="dateClass">${res.doc.data().date} </span> <button class="edit btn btn-default">edit</button> <button class="delete btn btn-danger">delete</button> </li>`)
-        });
-    }).catch(err => console.log(err))
+    getData()
+
+
+
+    function getData() {
+        table.empty()
+        db.get().then(result => {
+            let changes = result.docChanges() //gets array of docs
+            console.log(changes)
+            changes.forEach(res => {
+                console.log(res.doc.data());
+                // resList.append(`<li data-id="${res.doc.id}" class="li-look d-flex justify-content-lg-between px-3 py-1"> 
+                // <span class="nameClass">${res.doc.data().name} </span> - 
+                // <span class="locationClass"> ${res.doc.data().location} </span> -
+                //  <span class="dateClass">${res.doc.data().date} </span>
+                //  <button class="edit btn btn-default" data-toggle="modal" data-target="#popUp">edit</button> 
+                //  <button class="delete btn btn-danger">delete</button> </li>`)
+
+
+                table.append(`  <tr id="${res.doc.id}" data-id="${res.doc.id}">
+            <th scope="row" class="nameClass">${res.doc.data().name}</th>
+            <td class="locationClass" >${res.doc.data().location}</td>
+            <td  class="dateClass" > ${res.doc.data().date}  </td>
+            <td>  
+               <button class="edit btn btn-info" data-toggle="modal" data-target="#popUp">edit</button> 
+               <button class="delete btn btn-danger">delete</button> 
+            </td>     
+        </tr>`)
+
+
+            });
+        }).catch(err => console.log(err))
+
+    }
 
 
     db.onSnapshot(result => {
@@ -41,70 +69,48 @@ $(document).ready(function () { //must always be here if you use JQuery
     })
 
 
-    resList.on('click', ".delete", function () {
-        //$(this).parent().attr("data-id")
-        let id = $(this).parent().data("id")
+    table.on('click', ".delete", function () {
+        //  console.log(id)
+        let id = $(this).closest('tr').data("id")
+        console.log(id)
+
 
         db.doc(id).delete()
-        // .then()
-        $(this).parent().hide()
+        // // .then()
+        $(this).parent().parent().hide()
 
     })
 
-    
 
-    resList.on('click', ".edit", function () {
+
+    table.on('click', ".edit", function () {
         // $(this).parent().attr("data-id")
-        let id = $(this).parent().data("id")
-
-        
-
-        let nameI = $('input[name=name]').val()
-        let locationI = $('input[name=location]').val()
-
-
+        let id = $(this).closest('tr').data("id")
+        $('.submit').replaceWith(`<button class="update btn btn-info w-100 mt-2">تحديث</button>`)
         let date = $('input[name=bday]').val()
         db.doc(id).get().then(res => {
             // console.log(res.data());            
             $('input[name=name]').val(res.data().name)
-            $('input[name=location]').val(res.data().location)
+            $('input[select=location]').val(res.data().location)
             $('input[name=bday]').val(res.data().date)
-            $('.formcontainer').append(`<button class="update">Update</button>`)
-
-
+            // $('.formcontainer').append(`<button class="update">Update</button>`)
+            // $('.submit').replaceWith(`<button class="update btn btn-info w-100 mt-2">تحديث</button>`)
             $('.update').click(function () {
-                
-        let nameI = $('input[name=name]').val()
-        let locationI = $('input[name=location]').val()
-        let dateI = $('input[name=bday]').val()
-
-
+                let nameI = $('input[name=name]').val()
+                let locationI = $('select[name=location]').val()
+                let dateI = $('input[name=bday]').val()
                 db.doc(id).update({
                     name: nameI,
                     location: locationI,
-
-
-                })
-                    .then(docRef => {
-
-console.log($(`[data-id="${id}"]`));
-
-
+                    date: dateI,
+                }).then(docRef => {
                     $(".update").remove();
-                    
-                    $(`[data-id="${id}"]`).replaceWith(`<li data-id="${id}" class="li-look d-flex justify-content-lg-between p-3 "> <span class="nameClass" > ${nameI} </span> - <span class="locationClass"> ${locationI} </span>- <span class="locationClass"> ${dateI} </span> <button class="edit btn btn-default">edit</button> <button class="delete btn btn-danger">delete</button></li>`)
-                    
+                    getData()
                     $('input[name=name]').val('')
                     $('input[name=location]').val('')
                     $('input[name=bday]').val('')
-                    })
-
-
-
-                    
-
-                    
-
+                    done()
+                })
             })
         })
 
@@ -112,31 +118,54 @@ console.log($(`[data-id="${id}"]`));
 
     })
 
+    $('button[name=close]').click(function () {
+        $('input[name=name]').val('')
+        $('input[name=location]').val('')
+        $('input[name=bday]').val('')
+        $('.update').replaceWith(`<button class="submit btn btn-primary w-100 mt-2">احجز</button>`)
+    })
+
+
     //create data and store to restaurants collection
     $('.submit').click(function () {
-
         let nameI = $('input[name=name]').val()
-        let locationI = $('input[name=location]').val()
+        let locationI = $('select[name=location]').val()
         let dateI = $('input[name=bday]').val()
-
-
-
         db.add({
             name: nameI,
             location: locationI,
             date: dateI
+
         }).then(res => {
-
-            console.log(` ${name} - ${locationI}`);
-
-            resList.append(`<li data-id="${res.id}" class="li-look d-flex justify-content-lg-between px-3"> <span class="nameClass" > ${nameI} </span>- <span class="locationClass"> ${locationI} </span>- <span class="locationClass"> ${dateI} </span> <button class="edit btn btn-default">edit</button> <button class="delete btn btn-danger">delete</button></li>`)
+            getData()
         })
 
         $('input[name=name]').val('')
         $('input[name=location]').val('')
         $('input[name=bday]').val(currentDate)
-
-
+        done()
     })
-
 })
+
+$('#remove-table').click(function(){
+    $('.table').removeClass('show')
+    $('.table').addClass('hide')    
+    $('.calendar').addClass('show')
+    $('.calendar').removeClass('hide')    
+    // $('.calendar').show()
+})
+
+
+$('#remove-calender').click(function(){
+    $('.table').addClass('show')
+    $('.table').removeClass('hide')       
+    $('.calendar').removeClass('show')
+    $('.calendar').addClass('hide')
+})
+
+
+
+function done(){
+    $('.formcontainer').hide()
+    $('.done').addClass('show')
+}
