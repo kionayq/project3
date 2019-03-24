3c2c1af9b1d61885977dc2e2003826ad12a9b803
abinonayq <abinonayq@elm.sa>
2019-03-24 18:18:04 +0300

    DarkSky

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ $(document).ready(function () { //must always be here if you use JQuery
     let contContainer = $('.content-container')
     let table = $('#data-grid')
     db.where('date', '==', '2019-03-21').get()
-    .then(e => console.log(e.docChanges()[0].doc.data().location))
+        .then(e => console.log(e.docChanges()[0].doc.data().location))
     let calendarDiv = $('#calendar')
-   
+
     var tdate = new Date();
     var dd = tdate.getDate();
     var MM = ((tdate.getMonth().length + 1) === 1) ? (tdate.getMonth() + 1) : '0' + (tdate.getMonth() + 1);
     var yyyy = tdate.getFullYear();
     var currentDate = yyyy + "-" + MM + "-" + dd;
-    $('input[name=bday]').attr("placeholder",currentDate)
+    $('input[name=bday]').attr("placeholder", currentDate)
 
     // console.log(currentDate)
 
@@ -37,7 +37,7 @@ $(document).ready(function () { //must always be here if you use JQuery
                <button class="delete btn btn-danger">delete</button> 
             </td>     
         </tr>`)
-       
+
 
             });
 
@@ -118,214 +118,229 @@ $(document).ready(function () { //must always be here if you use JQuery
         $('input[name=name]').val('')
         $('input[name=location]').val('')
         $('input[name=bday]').val('')
-        $('.update').replaceWith(`<button class="submit btn btn-primary w-100 mt-2">احجز</button>`)
+        $('.update').replaceWith(`<button class="confirme btn btn-primary w-100 mt-2">احجز</button>`)
         formappears()
     })
-
+    $('#book').click(function () {
+        $('input[name=name]').val('')
+        $('input[name=location]').val('')
+        $('input[name=bday]').val('')
+        $('.update').replaceWith(`<button class="confirme btn btn-primary w-100 mt-2">احجز</button>`)
+        formappears()
+    })
 
     //create data and store to restaurants collection
     $('.submit').click(function () {
         let nameI = $('input[name=name]').val()
         let locationI = $('select[name=location]').val()
         let dateI = $('input[name=bday]').val()
+        let outdoorI = $('select[name=outdoor]').val()
 
-        db.where('date', '==', '2019-03-29').get()
-        .then(e => {
-            if(e.docChanges().length < 1){
-                db.add({
-                    name: nameI,
-                    location: locationI,
-                    date: dateI    
-                }).then(res => {
-                    getData()       
-                    done()
-                $('input[name=name]').val('')
-                $('input[name=location]').val('')
-                $('input[name=bday]').val(currentDate)
-                $('#bday').css('border-bottom','1px solid #757575')
-                $('.error').css('display','none')
-                })    
-            }else {
-                // alert("محجوز من زماان")                   
-                $('#bday').css('border-bottom',' 1px solid red')
-                $('.error').css('display','block')
-            }
-            
-
+        db.add({
+            name: nameI,
+            location: locationI,
+            date: dateI,
+            outdoor: outdoorI,
+        }).then(res => {
+            getData()
+            done()
+            $('input[name=name]').val('')
+            $('input[name=location]').val('')
+            $('input[name=bday]').val(currentDate)
+            $('#bday').css('border-bottom', '1px solid #757575')
+            $('.error').css('display', 'none')
         })
 
-                     
 
-      
-       
+
     })
 
-    function checkDate(input){
-        let checker = 0
-        console.log("input", input)
-        
-        db.get().then(result => {
-            let changes = result.docChanges() //gets array of docs
-            // console.log(result)
-            changes.forEach(res => { 
-                    if (res.doc.data().date == input){
-                        console.log("date", res.doc.data().date)                      
 
-                        // alert("there is a previuse boking")                        
-                        checker = 1
-                    }
-                })       
 
-                alert(checker)
-                if (checker == 1 ){                    
-                    return false                        
-                }else{                                  
-                    return true}                                
-            })
-          
-                      
+
+
+
+
+function checkDate(input) {
+    let checker = 0
+    console.log("input", input)
+
+    db.get().then(result => {
+        let changes = result.docChanges() //gets array of docs
+        // console.log(result)
+        changes.forEach(res => {
+            if (res.doc.data().date == input) {
+                console.log("date", res.doc.data().date)
+
+                // alert("there is a previuse boking")                        
+                checker = 1
+            }
+        })
+
+        alert(checker)
+        if (checker == 1) {
+            return false
+        } else {
+            return true
+        }
+    })
+
+
 }
 
-    $('#remove-table').click(function () {
-        // $('.calendar').empty()
-        // $('.content-container').empty()
-        //  getData()
-        // $('.content-container').append(calendarDiv)
-        
+$('#remove-table').click(function () {
 
-        $('.table').removeClass('show')
-        $('.table').addClass('hide')
-        $('.calendar').addClass('show')
-        $('.calendar').removeClass('hide')
-        $('.calendar').show()
+    $('.table').removeClass('show')
+    $('.table').addClass('hide')
+    $('.calendar').addClass('show')
+    $('.calendar').removeClass('hide')
+    $('.calendar').show()
+})
+
+
+
+
+$('#remove-table').click(removeTable())
+
+function removeTable() {
+    $('#table').removeClass('show')
+    $('#table').addClass('hide')
+    $('.calendar').addClass('show')
+    $('.calendar').removeClass('hide')
+}
+
+
+
+$('#remove-calender').click(function () {
+    $('.table').addClass('show')
+    $('.table').removeClass('hide')
+    $('.calendar').removeClass('show')
+    $('.calendar').addClass('hide')
+
+});
+
+
+
+// $('#bday').datepicker({
+//     format: 'yyyy-mm-dd',
+//     language: 'AR'
+// });
+
+
+// $('#bday').calendarsPicker(,$.extend( 
+//     {calendar: $.calendars.instance('islamic', 'ar')}, 
+//     $.calendarsPicker.regionalOptions['ar']));
+
+
+// $('#bday').calendarsPicker($.extend( 
+//     {calendar: $.calendars.instance('islamic', 'ar')}, 
+//     $.calendarsPicker.regionalOptions['ar']));
+
+$('#bday').calendarsPicker({
+    dateFormat: 'yyyy-mm-dd',
+});
+
+
+$('.confirme').click(function () {
+
+    let dateI = $('input[name=bday]').val()
+
+    db.where('date', '==', dateI).get()
+        .then(e => {
+            if (e.docChanges().length < 1) {
+                confirmed()
+            } else {
+                // alert("محجوز من زماان")                   
+                $('#bday').css('border-bottom', ' 1px solid red')
+                $('.error').css('display', 'block')
+            }
+
+        })
     })
 
-
-
-
-    $('#remove-table').click(removeTable())
-
-    function removeTable() {
-        $('#table').removeClass('show')
-        $('#table').addClass('hide')
-        $('.calendar').addClass('show')
-        $('.calendar').removeClass('hide')
+    function formappears() {
+        $('.formcontainer').show()
+        $('#weather').addClass('hide')
+        $('.done').removeClass('show')
     }
-
-
-
-    $('#remove-calender').click(function(){
-        $('.table').addClass('show')
-        $('.table').removeClass('hide')
-        $('.calendar').removeClass('show')
-        $('.calendar').addClass('hide')
-    });
-
-    // $('#remove-calender').click(removeCalender());
-
-    // function removeCalender(){
-    //     $('#table').addClass('show')
-    //     $('#table').removeClass('hide')
-    //     $('.calendar').removeClass('show')
-    //     $('.calendar').addClass('hide')
-    // }
-
-
-    // $('#bday').datepicker({
-    //     format: 'yyyy-mm-dd',
-    //     language: 'AR'
-    // });
-
-
-    // $('#bday').calendarsPicker(,$.extend( 
-    //     {calendar: $.calendars.instance('islamic', 'ar')}, 
-    //     $.calendarsPicker.regionalOptions['ar']));
-
-
-        // $('#bday').calendarsPicker($.extend( 
-        //     {calendar: $.calendars.instance('islamic', 'ar')}, 
-        //     $.calendarsPicker.regionalOptions['ar']));
-            
-        $('#bday').calendarsPicker({ 
-                dateFormat: 'yyyy-mm-dd',
-            });
-    
-
-   
-
-  
 
     function done() {
         $('.formcontainer').hide()
         $('.done').addClass('show')
+        $('#weather').removeClass('show')
+        $('#weather').addClass('hide')
     }
-    function formappears() {
-        $('.formcontainer').show()
-        $('.done').removeClass('show')
+
+    function confirmed() {
+        $('.formcontainer').hide()
+        $('.done').removeClass('hide')
+        $('#weather').addClass('show')
+        $('#weather').removeClass('hide')
+
     }
+
+
 
 
     //  full calender //
 
 
 
-    
-    function setCalendar(){
+
+    function setCalendar() {
         // calendarEl.empty()
-
         var calendarEl = document.getElementById('calendar');
-        var calendar ;
+        var calendar;
         eventss = [];
-    db.get().then(result => {
-        let changes = result.docChanges() //gets array of docs
-        changes.forEach(res => {
-            eventss.push({
-                title: `${res.doc.data().name}`,
-                start: `${res.doc.data().date}`,
-                allDay: true,                
+        db.get().then(result => {
+            let changes = result.docChanges() //gets array of docs
+            changes.forEach(res => {
+                eventss.push({
+                    title: `${res.doc.data().name}`,
+                    start: `${res.doc.data().date}`,
+                    allDay: true,
+                });
             });
-        });
-        calendar = new FullCalendar.Calendar(calendarEl, { selectable: true, 
-            dateClick: function(info) {
-           
-            //.attr("id", "popUp");
-            console.log(info.dateStr)
-            // let did = data("id")
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                selectable: true,
+                dateClick: function (info) {
+                    //.attr("id", "popUp");
+                    console.log(info.dateStr)
+                    // let did = data("id")
 
-            // $(this).modal()
-            $('input[name=bday]').val(info.dateStr)
-            $('#popUp').modal({show : true }) 
+                    // $(this).modal()
+                    $('input[name=bday]').val(info.dateStr)
+                    $('#popUp').modal({ show: true })
 
-            },
-        
-            locale: 'ar-sa',
-            plugins: ['interaction', 'dayGrid', 'timeGrid'],
-            defaultView: 'dayGridMonth',
-            defaultDate: currentDate,
-            timeZone: 'UTC',
-            code: "ar-sa",
-            header: {
-                left: 'prev',
-                center: 'title',
-                right: 'next'
-            },
-            events: eventss
-        });     
+                },
 
-
-
-        calendar.render();
+                locale: 'ar-sa',
+                plugins: ['interaction', 'dayGrid', 'timeGrid'],
+                defaultView: 'dayGridMonth',
+                defaultDate: currentDate,
+                timeZone: 'UTC',
+                code: "ar-sa",
+                header: {
+                    left: 'prev',
+                    center: 'title',
+                    right: 'next'
+                },
+                events: eventss
+            });
 
 
 
+            calendar.render();
 
-    }).catch(err => console.log(err))
 
-}
+
+
+        }).catch(err => console.log(err))
+
+    }
 
     // console.log(eventss)
 
-
-
-  
 })
+
+
