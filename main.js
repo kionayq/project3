926d07fdd77b1954a753fb1b4a748fabd7c61e71
unknown <kionayq@gmail.com>
2019-03-27 13:59:37 +0300

    و

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ $(document).ready(function () { //must always be here if you use JQuery
         $('.modal-title').html("")           
             db.doc(id).delete() 
             
-            // getData()
+            getData()
         })
 
 
